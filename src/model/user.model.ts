74cdb5eed430c664f16cf8a88be21be6c29d6696
akshat-khosya <akshatdps12@gmail.com>
2022-08-25 18:11:19 +0530

    host,port

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import config from "config";
+import config from "../../config/default";
 import bcrypt from "bcrypt";
 
 export interface UserDocument extends mongoose.Document {
